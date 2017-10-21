34bad98ce1cbe7dff4fffc6813bb1e47bdbf589e
Kazumasa <catter121@gmail.com>
2017-10-21 16:58:40 -0700

    add css

--- healthplusplus/settings.py
+++ healthplusplus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'code_search'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "code_search", "static", "code_search")
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, "code_search", "static", "code_search")
