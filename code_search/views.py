80bf438b4c5b5daa3d3d1358fe473982c14ffaa8
Kazumasa <catter121@gmail.com>
2017-10-21 16:31:23 -0700

    add template

--- code_search/views.py
+++ code_search/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('code_search/index.html')
+    context = {
+        'latest_question_list': "",
+    }
+    return HttpResponse(template.render(context, request))
--- healthplusplus/settings.py
+++ healthplusplus/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'healthplusplus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'code_search', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
