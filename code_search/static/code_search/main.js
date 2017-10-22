9a9f7680ce25e85f1880bfa2c3202f4eb405b1ad
Kazumasa <catter121@gmail.com>
2017-10-21 22:16:29 -0700

    add color to each treatment

--- code_search/static/code_search/main.js
+++ code_search/static/code_search/main.js
@@ -3,8 +3,30 @@ $(document).ready(function(){
   $(".button-collapse").sideNav({
     menuWidth: 200
   });
+  $('.on-edit').hide();
   $('#textarea1').trigger('autoresize');
   $('.submit').click(function () {
+    if ($('.on-edit').is(":visible")) {
+      $('.off-edit').empty();
+      // let label = $('<p></p>').text('Treatment');
+      let label = $('<label></label>').text('Treatment').attr('for','textarea1');//.addClass('label-off');
+      label.css('margin-top','-25px');
+      label.css('font-size','0.8rem');
+      $('.off-edit').append(label);
+      let content = $('#textarea1').text();
+      let sentenses = content.split("\n\n");
+      let colors = ['#b4dcea', '#fccae6', '#ffe4c2'];
+      for (let i in sentenses) {
+        let sentense = sentenses[i];
+        let p = $('<p></p>').append(sentense);
+        p.css('background-color',colors[i]);
+        $('.off-edit').append(p);
+      }
+    } 
+    $('.on-edit').toggle();
+    $('.off-edit').toggle();
+    // let div = $('<div></div>').addClass('styled-textarea materialize-textarea');
+    // $('.edit-col').append(div);
     $('.result').toggle();
   });
   $('.checkbox').click(function() {
