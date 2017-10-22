e309abcb98c93f5c6ea9bc89441df232fe76b8e6
Kazumasa <catter121@gmail.com>
2017-10-21 20:10:18 -0700

    add code selection

--- code_search/static/code_search/main.js
+++ code_search/static/code_search/main.js
@@ -1,10 +1,23 @@
 $(document).ready(function(){
+  Materialize.updateTextFields();
   $(".button-collapse").sideNav({
     menuWidth: 200
   });
   $('#textarea1').trigger('autoresize');
   $('.submit').click(function () {
     $('.result').toggle();
+  });
+  $('.checkbox').click(function() {
+    let code = $(this).data('code');
+    let selected_codes = $('#selected_codes').prop('value');
+    if ($(this).prop('checked')) {
+      let selected_codes_updates = selected_codes + code + ', ';
+      $('#selected_codes').attr('value', selected_codes_updates);
+    } else {
+      let reg_exp = new RegExp(code + ', ', "g");
+      let selected_codes_updates = selected_codes.replace(reg_exp, '');
+      $('#selected_codes').attr('value',selected_codes_updates);
+    }
   });
 });
 
